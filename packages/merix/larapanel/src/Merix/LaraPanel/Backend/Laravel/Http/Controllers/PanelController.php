d01cc9b478f1e0386df3c63fa8ee4c71f856ce91
h.narozny <h.narozny@merixstudio.com>
2016-07-19 08:36:04 +0200

    New: Panel Action Handling

--- packages/merix/larapanel/src/Merix/LaraPanel/Backend/Laravel/Http/Controllers/PanelController.php
+++ packages/merix/larapanel/src/Merix/LaraPanel/Backend/Laravel/Http/Controllers/PanelController.php
@@ -2,8 +2,11 @@
 
 namespace Merix\LaraPanel\Backend\Laravel\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Merix\LaraPanel\Core\Components\Action;
 use Merix\LaraPanel\Core\Contracts\LaraPanel;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class PanelController extends Controller
 {
@@ -35,9 +38,39 @@ class PanelController extends Controller
         return $response;
     }
 
-    public function action(LaraPanel $laraPanel, $panel, $action)
+    public function action(Request $request, LaraPanel $laraPanel, $panel, $action)
     {
-        return 'aaa';
+        $laraPanel->select($panel);
+        $panel = $laraPanel->getPanel();
+
+
+        $actions = $panel->getActions();
+        if(!isset($actions[$action]))
+        {
+            throw new NotFoundHttpException();
+        }
+
+        /** @var Action $action */
+        $action = $panel->getActions()[$action];
+        if(!$action->visible)
+        {
+            throw new NotFoundHttpException();
+        }
+
+        if(!$action->allowed)
+        {
+            \App::abort(403, 'Unauthorized action.');
+        }
+
+        $data = $request->input('data', []);
+
+        $handler = $action->handler;
+        if($handler != null)
+        {
+            $handler($panel, $data, $action);
+        }
+
+        return $action->getResponse();
     }
 
 }
